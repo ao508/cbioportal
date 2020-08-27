57cb19d20d0316fcbaa72da1fc9cad175cdfc73c
Angelica Ochoa <15623749+ao508@users.noreply.github.com>
2020-08-27 18:59:21 -0400

    Print resolved tumor seq allele values

--- core/src/main/java/org/mskcc/cbio/portal/scripts/ImportExtendedMutationData.java
+++ core/src/main/java/org/mskcc/cbio/portal/scripts/ImportExtendedMutationData.java
@@ -382,10 +382,10 @@ public class ImportExtendedMutationData{
                             record.getEndPosition() + "," + record.getReferenceAllele() + "," + record.getTumorSeqAllele1() +
                             "," + record.getTumorSeqAllele2() + ")");
                     } else {
-                        ProgressMonitor.logWarning("Tumor allele resolved to: '" + sample.getStableId() +
-                            "' (chr,start,end,ref,tum1,tum2) = (" + record.getChr() + "," + record.getStartPosition() + "," +
+                        System.out.println("Tumor allele resolved to: '" + sample.getStableId() +
+                            "' (chr,start,end,ref,tum1,tum2,tumseqallele) = (" + record.getChr() + "," + record.getStartPosition() + "," +
                             record.getEndPosition() + "," + record.getReferenceAllele() + "," + record.getTumorSeqAllele1() +
-                            "," + record.getTumorSeqAllele2() + ")");
+                            "," + record.getTumorSeqAllele2() + "," + mutation.getTumorSeqAllele() + ")");
 		    }
 
                     mutation.setDbSnpRs(record.getDbSNP_RS());
